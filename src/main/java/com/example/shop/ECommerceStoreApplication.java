72dcdf6a5f4aa1083abddecf2f7011791179d030
VovaTsar <dropttecttt@mail.ru>
2019-05-10 15:24:43 +0300

    start

---
+++ src/main/java/com/example/shop/ECommerceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceStoreApplication.class, args);
+    }
+
+}
